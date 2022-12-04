84cd055afee61d951c21c6555f10a973ccc141c3
Stanley Lee <stanleylee91776@gmail.com>
2022-12-04 14:09:35 -0800

    FS Fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
 const withImages = require("next-images");
+
+module.exports = {
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+  
+      return config
+    }
+  }
+
 module.exports = withImages();
