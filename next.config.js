4f69047043b2bea5a5df898022a532790898393e
Stanley Lee <stanleylee91776@gmail.com>
2022-12-04 14:12:39 -0800

    FS Fix

--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@
 const withImages = require("next-images");
 
 module.exports = {
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty'
-        }
-      }
+    future: {
+      webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
+        // Looks like backward compatibility approach.
+    },
+    webpack(config) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+          // by next.js will be dropped. Doesn't make much sense, but how it is
+        fs: false, // the solution
+      };
   
-      return config
-    }
-  }
+      return config;
+    },
+  };
 
 module.exports = withImages();
