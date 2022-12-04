8066bf2a8c8a307f9c058ad46cc5388fe34626f5
Emily Nguyen <emilyhuongnguyen@gmail.com>
2022-12-03 16:52:08 -0800

    Bootstrap pages and add in SASS

---
+++ pages/ingame.js
@@ -0,0 +1,9 @@
+import styles from '../styles/InGame.module.scss'
+
+export default function InGame() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.testHeading1}>In Game</h1>
+    </div>
+  )
+}
---
+++ pages/postgame.js
@@ -0,0 +1,9 @@
+import styles from '../styles/PostGame.module.scss'
+
+export default function PostGame() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.testHeading2}>Post Game</h1>
+    </div>
+  )
+}
