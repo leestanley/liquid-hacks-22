c0e70907a0f03feb7b2c927523dbbd12bb243c8b
Emily Nguyen <emilyhuongnguyen@gmail.com>
2022-12-04 05:40:12 -0800

    Add icons

--- pages/components/postgame/analytics/heartrate.jsx
+++ pages/components/postgame/analytics/heartrate.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeartRateIcon from './assets/heartrate_icon.svg';
 
 import styles from '../../../../styles/components/postgame/analytics/HeartRate.module.scss';
 
 const HeartRate = ({ data }) => (
   <div className={styles.container}>
     <div className={styles.header}>
+      <img src={HeartRateIcon} alt="HeartRate Icon" />
       <h2>Average Heart Rate</h2>
       <p>75BPM per game</p>
     </div>
--- pages/components/postgame/analytics/toxicity.jsx
+++ pages/components/postgame/analytics/toxicity.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ToxicIcon from './assets/toxic_icon.svg';
 
 import styles from '../../../../styles/components/postgame/analytics/Toxicity.module.scss';
 
 const Toxicity = ({ data }) => (
   <div className={styles.container}>
     <div className={styles.header}>
+      <img src={ToxicIcon} alt="Toxic Icon" />
       <h2>Toxicity Score</h2>
       <p>Based off voice recordings</p>
     </div>
