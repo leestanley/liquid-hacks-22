f5b4feb271b180d1853fbd5e4e00ad941242bde5
Stanley Lee <stanleylee91776@gmail.com>
2022-12-04 16:24:54 -0800

    Library fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,16 @@
 /** @type {import('next').NextConfig} */
 const withImages = require("next-images");
 
-module.exports = {
-    future: {
-      webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-        // Looks like backward compatibility approach.
-    },
-    webpack(config) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-          // by next.js will be dropped. Doesn't make much sense, but how it is
-        fs: false, // the solution
-      };
-  
-      return config;
-    },
-  };
+module.exports = withImages({
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
 
-module.exports = withImages();
+        return config;
+    }
+});
+
