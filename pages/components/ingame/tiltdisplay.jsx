0fc1bf185f77ec218d65e07aef85c054fa227e97
Emily Nguyen <emilyhuongnguyen@gmail.com>
2022-12-04 20:54:02 -0800

    More changes

--- pages/components/ingame/tiltdisplay.jsx
+++ pages/components/ingame/tiltdisplay.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import styles from '../../../styles/components/ingame/TiltDisplay.module.scss';
+import BrainIcon from './assets/brain.svg';
 
 const TiltDisplay = (props) => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>Live Tilt Health</h2>
+      </div>
+      <div className={styles.status}>
+          <img src={BrainIcon} />
+          <div>
+            <h2>Poor</h2>
+            <p>Reduce your use of profanity. Whether it is said to your teammates or while you are on mute, this can decrease your level of play.</p>
+          </div>
+        </div>
     </div>
   );
 }
