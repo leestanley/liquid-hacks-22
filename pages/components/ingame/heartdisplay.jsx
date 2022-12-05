7198190dbbd1089ad8ce8578df59adea9954c4ff
Emily Nguyen <emilyhuongnguyen@gmail.com>
2022-12-04 19:02:17 -0800

    Add containers for ingame elements

---
+++ pages/components/ingame/heartdisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../../../styles/components/ingame/HeartDisplay.module.scss';
+
+const HeartDisplay = (props) => {
+
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
+
+export default HeartDisplay;
---
+++ pages/components/ingame/tiltdisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../../../styles/components/ingame/TiltDisplay.module.scss';
+
+const TiltDisplay = (props) => {
+
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
+
+export default TiltDisplay;
