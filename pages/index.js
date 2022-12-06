e03a834f35ce9e953d10c2dfb1034c89251e951d
Emily Nguyen <emilyhuongnguyen@gmail.com>
2022-12-06 03:03:06 -0800

    Add modal

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Image from "next/image";
 
 import Points from '../public/points.svg'
 
-const displayModal = false;
+const displayModal = true;
 //const displayModal = process.env.NODE_ENV === 'production';
 
 export default function Home() {
